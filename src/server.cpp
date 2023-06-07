25012e48757ed7787e2f6bf9d99c9d30cc5b2701
bsomers42 <brittsomers22@gmail.com>
2023-06-07 16:18:50 +0200

    Now works with multiplexing

--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,16 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/event.h>
+#include <fstream>
+
+#define NUSERS 10
+
+struct uc { //this should go in the class!!
+    int uc_fd;
+    char *uc_addr;
+} users[NUSERS];
+
 
 bool	write_exit(std::string error)
 {
@@ -11,14 +21,117 @@ bool	write_exit(std::string error)
 	return (false);
 }
 
+/* find the index of a file descriptor or a new slot if fd=0 */
+int	conn_index(int fd) 
+{
+    int uidx;
+    for (uidx = 0; uidx < NUSERS; uidx++)
+        if (users[uidx].uc_fd == fd)
+            return uidx;
+    return -1;
+}
+
+int conn_add(int fd) 
+{
+    int uidx;
+    if (fd < 1) return -1;
+    if ((uidx = conn_index(0)) == -1)
+        return -1;
+    if (uidx == NUSERS) {
+        close(fd);
+        return -1;
+    }
+    users[uidx].uc_fd = fd; /* users file descriptor */
+    users[uidx].uc_addr = 0; /* user IP address */
+    return 0;
+}
+
+void	watch_loop(int kq, int listenfd)
+{
+	struct kevent evSet;
+	struct kevent evList[32];
+	int nev, i;
+	struct sockaddr_storage addr;
+	socklen_t socklen = sizeof(addr);
+	int fd;
+	while (1)
+	{
+		nev = kevent(kq, NULL, 0, evList, 32, NULL);
+		if (nev < 1)
+		{
+			write_exit("kevent error");
+			return ;
+		}
+		for (i = 0; i<nev; i++)
+		{
+			if (evList[i].flags & EV_EOF)
+			{
+				printf("Disconnect\n");
+				fd = evList[i].ident;
+				EV_SET(&evSet, fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+				if (kevent(kq, &evSet, 1, NULL, 0, NULL) == -1)
+				{
+					write_exit("kevent error");
+					return ;
+				}
+				close(fd);
+			}
+			else if ((int)evList[i].ident == listenfd)
+			{
+				printf("Here1\n");
+
+				fd = accept(evList[i].ident, (struct sockaddr *)&addr, &socklen);
+				if (fd == -1)
+				{
+					write_exit("accept error");
+					return ;
+				}
+				if (conn_add(fd) == 0)
+				{
+					EV_SET(&evSet, fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+					if (kevent(kq, &evSet, 1, NULL, 0, NULL) == -1)
+					{
+						write_exit("accept error");
+						return ;
+					}
+					uint8_t buff[MAXLINE + 1];
+					//snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK \r\nContent-Type: text/html\r\nContent-Length: 20\r\n\r\nWe socket thisssssss"); //can write formatted output to sized buf
+					std::string   fileBuf;
+					std::string line;
+					std::ifstream   htmlFile;
+					htmlFile.open("data/index.html");
+					while (std::getline (htmlFile, line))
+						fileBuf += line;
+					printf("Len: %lu\n", fileBuf.length());
+					snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK \r\nContent-Type: text/html\r\nContent-Length: %lu\r\n\r\n%s", fileBuf.length(), fileBuf.c_str());
+					write(fd, (char*)buff, std::strlen((char*)buff));
+					htmlFile.close();
+				}
+				else
+				{
+					printf("Connection refused\n");
+					close(fd);
+				}
+			}
+			else if (evList[i].filter == EVFILT_READ)
+			{
+				char buf[256];
+				size_t bytes_read;
+
+				bytes_read = recv(evList[i].ident, buf, sizeof(buf), 0);
+				if ((int)bytes_read < 0)
+					printf("%d bytes read\n", (int)bytes_read);
+			}
+		}
+	}
+}
+
 bool	Server::start()
 {
 	if (this->running)
 		return (false);
-	int		listenfd, connfd, n; //connfd will actually talk to the client that's connected
+	int		listenfd;//.n;//connfd; //connfd will actually talk to the client that's connected
 	struct	sockaddr_in	servaddr;
-	uint8_t buff[MAXLINE + 1];
-	uint8_t recvline[MAXLINE + 1];
 
 	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) //AF_INET = internet socket, SOCK_STREAM = tcp stream
 		return (write_exit("socket error"));
@@ -32,28 +145,13 @@ bool	Server::start()
 		return (write_exit("bind error"));
 	if ((listen(listenfd, 10)) < 0)
 		return (write_exit("listen error"));
-
-	while(1)
-	{
-		// struct sockaddr_in	addr;
-		// socklen_t	addr_len;
-		//accept blocks until an incoming connection arrives. returns an fd to the connection
-		std::cout << "Waiting for a connection on port " << SERVER_PORT << std::endl;
-		connfd = accept(listenfd, (SA *) NULL, NULL); //set to NULL because doesn't matter who connects, just accept
-		std::memset(recvline, 0, MAXLINE);
-		while ((n = read(connfd, recvline, MAXLINE - 1)) > 0)
-		{
-			std::cout << "\n" << recvline << std::endl;
-			if (recvline[n - 1] == '\n')
-				break ;
-		}
-		std::memset(recvline, 0, MAXLINE);
-		if (n < 0) //can't read negative bytes
-			return (write_exit("read error"));
-		snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK \r\n\r\nWe socket this"); //can write formatted output to sized buf
-		write(connfd, (char*)buff, std::strlen((char*)buff));
-		close(connfd);
-	}
+	int kq;
+	struct kevent evSet;
+	kq = kqueue();
+	EV_SET(&evSet, listenfd, EVFILT_READ, EV_ADD, 0, 0, NULL);//EV_SET is a macro that fills the kevent struct
+	if (kevent(kq, &evSet, 1, NULL, 0, NULL) == -1)
+		return (write_exit("kqueue/kevent error"));
+	watch_loop(kq, listenfd);
 	return (true);	
 }
 
