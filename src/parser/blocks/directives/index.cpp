52065d797bba77faa2a73e95890068bce3f5d734
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-07-03 17:29:49 +0200

    index parsing improved, line will be split into multiple indexes

--- src/parser/blocks/directives/index.cpp
+++ src/parser/blocks/directives/index.cpp
@@ -12,6 +12,13 @@ t_values	parseIndex(std::string &line, t_values values)
 {
 	line = protectedSubstr(line, 5);
 	line = ltrim(line);
-	values.indexes.push_back(line);
+	while (findFirstWhitespace(line) != line.size() && line != "" && findFirstWhitespace(line) != 0)
+	{
+		values.indexes.push_back(protectedSubstr(line, 0, findFirstWhitespace(line)));
+		line = protectedSubstr(line, findFirstWhitespace(line) + 1);
+		line = ltrim(line);
+	}
+	if (line != "")
+		values.indexes.push_back(line);
 	return (values);
 }
