b3ed3ff343f55d8cccb3f028da1a541f14e1cac7
bsomers42 <brittsomers22@gmail.com>
2023-07-12 14:38:00 +0200

    Added fcntl and extra EV_SET in socket

--- src/server.cpp
+++ src/server.cpp
@@ -7,14 +7,7 @@
 #include <unistd.h>
 #include <sys/event.h>
 #include <fstream>
-
-// #define NUSERS 10
-
-// struct uc { //this should go in the class!!
-//     int uc_fd;
-//     char *uc_addr;
-// } users[NUSERS];
-
+#include <fcntl.h>
 
 #include "Request.hpp"
 #include "Response.hpp"
@@ -25,72 +18,6 @@ bool	Webserver::write_exit(std::string error)
 	std::cout << "ERROR: " << error << std::endl;
 	return (false);
 }
-
-// /* find the index of a file descriptor or a new slot if fd=0 */
-// int	Socket::connIndex(int fd) 
-// {
-//     int uidx;
-//     for (uidx = 0; uidx < NUSERS; uidx++)
-//         if (users[uidx].uc_fd == fd)
-//             return uidx;
-//     return -1;
-// }
-
-// int Socket::connAdd(int fd) 
-// {
-//     int uidx;
-//     if (fd < 1) return -1;
-//     if ((uidx = connIndex(0)) == -1)
-//         return -1;
-//     if (uidx == NUSERS) {
-//         close(fd);
-//         return -1;
-//     }
-//     users[uidx].uc_fd = fd; /* users file descriptor */
-//     users[uidx].uc_addr = 0; /* user IP address */
-//     return 0;
-// }
-
-// /* remove a connection and close its fd */
-// int Socket::connDelete(int fd) {
-//     int uidx;
-//     if (fd < 1) 
-// 		return -1;
-//     if ((uidx = connIndex(fd)) == -1)
-//         return -1;
-
-//     users[uidx].uc_fd = 0;
-//     users[uidx].uc_addr = NULL;
-
-//     /* free(users[uidx].uc_addr); */
-//     return close(fd);
-// }
-
-// bool	Socket::setUpConn(int kq, struct kevent evSet)
-// {
-// 	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) //AF_INET = internet socket, SOCK_STREAM = tcp stream
-// 		return (write_exit("socket error"));
-// 	int reuse; //this and setsockopt avoids the bind error and allows to reuse the address
-// 	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(int)) == -1)
-//  		return(write_exit("reuse port error"));
-// 	//setting up address you're listening on
-// 	std::memset(&servAddr, '\0', sizeof(servAddr));
-// 	servAddr.sin_family		= AF_INET;
-// 	servAddr.sin_addr.s_addr = htonl(INADDR_ANY); // will respond to anything
-// 	printf("port: %d\n", this->port);
-// 	servAddr.sin_port		= htons(/*SERVER_PORT*/port); //port you're listening on
-// 	if ((bind(listenfd, (SA *) &servAddr, sizeof(servAddr))) < 0)//bind listening socket to address
-// 		return (write_exit("bind error"));
-// 	if ((listen(listenfd, 10)) < 0)
-// 		return (write_exit("listen error"));
-// 	// kq = kqueue();
-// 	EV_SET(&evSet, listenfd, EVFILT_READ, EV_ADD, 0, 0, NULL);//EV_SET is a macro that fills the kevent struct
-// 	if (kevent(kq, &evSet, 1, NULL, 0, NULL) == -1)
-// 		return (write_exit("kqueue/kevent error"));
-// 	// watchLoop();
-// 	return (true);	
-
-// }
 
 int		Webserver::comparefd(std::vector<Socket> sckts, int eventfd)
 {
@@ -109,13 +36,11 @@ void	Webserver::start(std::vector<Server> servers)
 		return ;//(false);
 	for (size_t i = 0; i < servers.size(); i++)
 	{
-		// for (size_t j = 0; j < servers.at(i).getListens().size(); j++)
-		// {
-			// servers.at(i).getListens.at(j).second;
-			Socket sock(servers.at(i).getPort(0));
-			// Socket sock(servers.at(i).getListens().at(j).second);
+		for (size_t j = 0; j < servers.at(i).getListens().size(); j++)
+		{
+			Socket sock(servers.at(i).getListens().at(j).second);
 			sckts.push_back(sock);
-		// }
+		}
 	}
 	int kq = kqueue();
 	struct kevent evSet;
@@ -125,6 +50,7 @@ void	Webserver::start(std::vector<Server> servers)
 	}
 	//watchLoop();
 	struct kevent evList[2];
+	//EV_LIST(&evList[0], )
 	int nev, i;
 	struct sockaddr_storage addr;
 	socklen_t socklen = sizeof(addr);
@@ -163,7 +89,7 @@ void	Webserver::start(std::vector<Server> servers)
 			}
 			else if (comparefd(sckts, (int)evList[i].ident) == 1)//(int)evList[i].ident == sckt1.listenfd || (int)evList[i].ident == sckt2.listenfd)
 			{
-				// printf("Here1\n");
+				printf("Here1\n");
 
 				fd = accept(evList[i].ident, (struct sockaddr *)&addr, &socklen);
 				if (fd == -1)
@@ -173,6 +99,11 @@ void	Webserver::start(std::vector<Server> servers)
 				}
 				// if (connAdd(fd) == 0)
 				// {
+					if (fcntl(fd, F_SETFL, O_NONBLOCK) < 0)
+					{
+						write_exit("fcntl error");
+						return ;
+					}
 					EV_SET(&evSet, fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
 					if (kevent(kq, &evSet, 1, NULL, 0, NULL) == -1)
 					{
@@ -198,6 +129,17 @@ void	Webserver::start(std::vector<Server> servers)
 					// 	delete response;
 					// }
 					delete newResp;
+				// uint8_t buff[MAXLINE + 1];
+				// 	//snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK \r\nContent-Type: text/html\r\nContent-Length: 20\r\n\r\nWe socket thisssssss"); //can write formatted output to sized buf
+				// 	std::string   fileBuf;
+				// 	std::string line;
+				// 	std::ifstream   htmlFile;
+				// 	htmlFile.open("data/www/index.html");
+				// 	while (std::getline (htmlFile, line))
+				// 		fileBuf += line;
+				// 	snprintf((char*)buff, sizeof(buff), "HTTP/1.0 200 OK \r\nContent-Type: text/html\r\nContent-Length: %lu\r\n\r\n%s", fileBuf.length(), fileBuf.c_str());
+				// 	write(fd, (char*)buff, std::strlen((char*)buff));
+				// 	htmlFile.close();
 			// }
 			// else
 			// {
--- src/socket.cpp
+++ src/socket.cpp
@@ -1,12 +1,6 @@
 #include "../include/socket.hpp"
 #include "../include/webserver.hpp"
-
-#define NUSERS 10
-
-struct uc { //this should go in the class!!
-    int uc_fd;
-    char *uc_addr;
-} users[NUSERS];
+#include <fcntl.h>
 
 Socket::Socket(unsigned short newport) : port(newport)
 {
@@ -24,46 +18,6 @@ bool	Socket::write_exit(std::string error)
 	return (false);
 }
 
-/* find the index of a file descriptor or a new slot if fd=0 */
-int	Socket::connIndex(int fd) 
-{
-    int uidx;
-    for (uidx = 0; uidx < NUSERS; uidx++)
-        if (users[uidx].uc_fd == fd)
-            return uidx;
-    return -1;
-}
-
-int Socket::connAdd(int fd) 
-{
-    int uidx;
-    if (fd < 1) return -1;
-    if ((uidx = connIndex(0)) == -1)
-        return -1;
-    if (uidx == NUSERS) {
-        close(fd);
-        return -1;
-    }
-    users[uidx].uc_fd = fd; /* users file descriptor */
-    users[uidx].uc_addr = 0; /* user IP address */
-    return 0;
-}
-
-/* remove a connection and close its fd */
-int Socket::connDelete(int fd) {
-    int uidx;
-    if (fd < 1) 
-		return -1;
-    if ((uidx = connIndex(fd)) == -1)
-        return -1;
-
-    users[uidx].uc_fd = 0;
-    users[uidx].uc_addr = NULL;
-
-    /* free(users[uidx].uc_addr); */
-    return close(fd);
-}
-
 bool	Socket::setUpConn(int kq, struct kevent evSet)
 {
 	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) //AF_INET = internet socket, SOCK_STREAM = tcp stream
@@ -73,15 +27,17 @@ bool	Socket::setUpConn(int kq, struct kevent evSet)
  		return(write_exit("reuse port error"));
 	//setting up address you're listening on
 	std::memset(&servAddr, '\0', sizeof(servAddr));
-	servAddr.sin_family		= AF_INET;
+	servAddr.sin_family		= AF_INET;//AF_INET is an address family that is used to designate the type of addresses that your socket can communicate with (in this case, IPv4 addresses)
 	servAddr.sin_addr.s_addr = htonl(INADDR_ANY); // will respond to anything
 	printf("port: %d\n", this->port);
-	servAddr.sin_port		= htons(/*SERVER_PORT*/port); //port you're listening on
+	servAddr.sin_port		= htons(port); //port you're listening on
 	if ((bind(listenfd, (SA *) &servAddr, sizeof(servAddr))) < 0)//bind listening socket to address
 		return (write_exit("bind error"));
 	if ((listen(listenfd, 10)) < 0)
 		return (write_exit("listen error"));
-	// kq = kqueue();
+    if (fcntl(listenfd, F_SETFL, O_NONBLOCK) < 0)
+   		return (write_exit("fcntl error"));
+	EV_SET(&evSet, listenfd, EVFILT_WRITE, EV_ADD, 0, 0, NULL);//EV_SET is a macro that fills the kevent struct
 	EV_SET(&evSet, listenfd, EVFILT_READ, EV_ADD, 0, 0, NULL);//EV_SET is a macro that fills the kevent struct
 	if (kevent(kq, &evSet, 1, NULL, 0, NULL) == -1)
 		return (write_exit("kqueue/kevent error"));
