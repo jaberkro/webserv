5ce616caa8927ee1c354e1789a939d6047984587
Jaberkro <jaberkro@student.codam.nl>
2023-07-21 12:55:38 +0200

    index needs at least one argument, each argument needs an extension. slash will be placed in front of each index when not defined in the config file

--- src/parser/blocks/directives/index.cpp
+++ src/parser/blocks/directives/index.cpp
@@ -12,13 +12,37 @@ t_values	parseIndex(std::string line, t_values values)
 {
 	line = protectedSubstr(line, 5);
 	line = ltrim(line);
+	if (line == "")
+	{
+		std::cout << "Error: index needs at least one argument: index <fileName>.<extension>;" << std::endl;
+		exit(EXIT_FAILURE);
+	}
 	while (findFirstWhitespace(line) != line.size() && line != "" && findFirstWhitespace(line) != 0)
 	{
-		values.indexes.push_back(protectedSubstr(line, 0, findFirstWhitespace(line)));
+		std::string newIndex = protectedSubstr(line, 0, findFirstWhitespace(line));
+		if (newIndex.find(".") == std::string::npos || newIndex.find(".") == newIndex.size() - 1)
+		{
+			std::cout << "Error: index argument needs to have a name and extension seperated by '.': index <fileName>.<extension>;" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		if (newIndex.find("/") != 0)
+			values.indexes.push_back("/" + newIndex);
+		else
+			values.indexes.push_back(newIndex);
 		line = protectedSubstr(line, findFirstWhitespace(line) + 1);
 		line = ltrim(line);
 	}
 	if (line != "")
-		values.indexes.push_back(line);
+	{
+		if (line.find(".") == std::string::npos || line.find(".") == line.size() - 1)
+		{
+			std::cout << "Error: index argument needs to have a name and extension seperated by '.': index <fileName>.<extension>;" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		if (line.find("/") != 0)
+			values.indexes.push_back("/" + line);
+		else
+			values.indexes.push_back(line);
+	}
 	return (values);
 }
