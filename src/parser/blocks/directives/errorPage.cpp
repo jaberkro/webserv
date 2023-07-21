001f11104defd1ea2674a6f0bbfc93c0c1e9cb44
Jaberkro <jaberkro@student.codam.nl>
2023-07-21 17:48:00 +0200

    parseErrorPages calls multiple helper functions for more structure

--- src/parser/blocks/directives/errorPage.cpp
+++ src/parser/blocks/directives/errorPage.cpp
@@ -1,6 +1,39 @@
 #include "parse.hpp"
 #include <iostream>
 
+static void codeError(std::string code)
+{
+	std::cout << "Error: can't parse error_page: [" << code << "]: not a valid error code" << std::endl;
+	exit(EXIT_FAILURE);
+}
+
+static t_values addErrorPage(std::string code, std::string file, t_values values)
+{
+	try 
+	{
+		if (!allDigits(code) || code.size() != 3 || !validErrorCode(stoi(code)))
+			codeError(code);
+		values.errorPages[stoi(code)] = file;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	return (values);
+}
+
+static void checkValidFilename(std::string filename)
+{
+	if (filename.find("/") != 0 || filename.find(".") == std::string::npos || \
+		filename.find(".") == filename.size() - 1)
+	{
+		std::cout << "Error: can't parse error_page: [" << filename << \
+			"]: filename should start with '/' and contain extension: /<name>.<extension" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
 static void checkEmptyString(std::string line)
 {
 	if (line == "")
@@ -15,61 +48,28 @@ static void checkEmptyString(std::string line)
  * 
  * @param line the line to parse
  * @param values the struct to update
- * @return t_values the updated struct containing the parsed error_page overwriting the already existing error_page
+ * @return t_values the updated struct containing the parsed error_page 
+ * overwriting the already existing error_page
  */
 t_values	parseErrorPage(std::string line, t_values values)
 {
-	std::string	value;
-	std::string	key;
+	std::string	filename;
 
 	line = protectedSubstr(line, 10);
 	line = ltrim(line);
 	checkEmptyString(line);
 	// values.errorPages.clear(); //outcomment this if error_page should overwrite itself
-	value = protectedSubstr(line, line.find_last_of(" \t\v\b") + 1, line.size() - line.find_last_of(" \t\v\b") + 1);
-	if (value.find("/") != 0 || value.find(".") == std::string::npos || value.find(".") == value.size() - 1)
-	{
-		std::cout << "Error: can't parse error_page: [" << value << "]: filename should start with '/' and contain extension: /<name>.<extension" << std::endl;
-		exit(EXIT_FAILURE);
-	}
+	filename = protectedSubstr(line, line.find_last_of(" \t\v\b") + 1, line.size() - line.find_last_of(" \t\v\b") + 1);
+	checkValidFilename(filename);
 	line = protectedSubstr(line, 0, line.find_last_of(" \t") + 1);
 	line = rtrim(line);
 	while (findFirstWhitespace(line) != line.size() && line != "" && findFirstWhitespace(line) != 0)
 	{
-		key = protectedSubstr(line, 0, findFirstWhitespace(line));
-		try 
-		{
-			if (!allDigits(key) || key.size() != 3 || !validErrorCode(stoi(key)))
-			{
-				std::cout << "Error: can't parse error_page: [" << key << "]: not a valid error code" << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			values.errorPages[stoull(key)] = value;
-		}
-		catch(const std::exception& e)
-		{
-			std::cout << e.what() << std::endl;
-			exit(EXIT_FAILURE);
-		}
+		values = addErrorPage(protectedSubstr(line, 0, findFirstWhitespace(line)), filename, values);
 		line = protectedSubstr(line, findFirstWhitespace(line) + 1);
 		line = ltrim(line);
 	}
 	if (line != "")
-	{
-		try 
-		{
-			if (!allDigits(line) || line.size() != 3 || !validErrorCode(stoi(line)))
-			{
-				std::cout << "Error: can't parse error_page: [" << line << "]: not a valid error code" << std::endl;
-				exit(EXIT_FAILURE);
-			}
-			values.errorPages[stoi(line)] = value;
-		}
-		catch(const std::exception& e)
-		{
-			std::cout << e.what() << std::endl;
-			exit(EXIT_FAILURE);
-		}
-	}
+		values = addErrorPage(line, filename, values);
 	return (values);
 }
