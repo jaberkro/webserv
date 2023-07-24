64e17367a41ef5d1dcd57e1427658f1f3a1cace1
Jaberkro <jaberkro@student.codam.nl>
2023-07-24 18:07:10 +0200

    all lines 80 characters or less, all functions max 25 lines

--- src/parser/blocks/directives/errorPage.cpp
+++ src/parser/blocks/directives/errorPage.cpp
@@ -28,32 +28,31 @@ static void checkEmptyString(std::string line)
  * 
  * @param line the line to parse
  * @param values the struct to update
- * @return t_values the updated struct containing the parsed error_page 
- * overwriting the already existing error_page
+ * @return t_values the updated struct containing the parsed error_page.
  */
 t_values	parseErrorPage(std::string line, t_values values)
 {
-	std::string	filename;
+	std::string	file;
 	std::string	code;
 	size_t		lastWhitespace;
 
 	line = protectedSubstr(line, 10);
 	line = ltrim(line);
 	checkEmptyString(line);
-	// values.errorPages.clear(); //outcomment this if error_page should overwrite itself
+	// values.errorPages.clear(); //if error_page should overwrite itself
 	lastWhitespace = line.find_last_of(" \t\v\b") + 1;
-	filename = protectedSubstr(line, lastWhitespace, line.size() - lastWhitespace);
-	checkValidFilename(filename);
+	file = protectedSubstr(line, lastWhitespace, line.size() - lastWhitespace);
+	checkValidFilename(file);
 	line = protectedSubstr(line, 0, line.find_last_of(" \t") + 1);
 	line = rtrim(line);
 	while (firstWhitespace(line) != line.size() && firstWhitespace(line) != 0)
 	{
 		code = protectedSubstr(line, 0, firstWhitespace(line));
-		values.errorPages[parseErrorCode(code, "error_page")] = filename;
+		values.errorPages[parseErrorCode(code, "error_page")] = file;
 		line = protectedSubstr(line, firstWhitespace(line) + 1);
 		line = ltrim(line);
 	}
 	if (line != "")
-		values.errorPages[parseErrorCode(line, "error_page")] = filename;
+		values.errorPages[parseErrorCode(line, "error_page")] = file;
 	return (values);
 }
