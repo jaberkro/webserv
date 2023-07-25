1f6be93886d89e5318eeca14c39e8c0c5b430d49
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-07-25 18:46:39 +0200

    check functions added that are more widely used now via the checkExit file

--- src/parser/blocks/directives/uploadDir.cpp
+++ src/parser/blocks/directives/uploadDir.cpp
@@ -1,48 +1,5 @@
 #include "parse.hpp"
 #include <string>
-#include <iostream>
-
-static void checkNoEndingSlash(std::string line)
-{
-	if (line.find_last_of("/") == line.size() - 1)
-	{
-		std::cout << "Error: can't parse upload_dir: ";
-		std::cout << "path should not end with '/': ";
-		std::cout << "[" << line << "]" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-}
-
-static void checkStartingSlash(std::string line)
-{
-	if (line.find("/") != 0)
-	{
-		std::cout << "Error: can't parse upload_dir: ";
-		std::cout << "path should start with '/': ";
-		std::cout << "[" << line << "]" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-}
-
-static void checkOneArgumentOnly(std::string line)
-{
-	if (firstWhitespace(line) != line.size())
-	{
-		std::cout << "Error: can't parse upload_dir: too many arguments: ";
-		std::cout << "[" << line << "]" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-}
-
-static void checkEmptyString(std::string line)
-{
-	if (line == "")
-	{
-		std::cout << "Error: upload_dir needs one argument: ";
-		std::cout << "upload_dir <path>;" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-}
 
 /**
  * @brief parse a upload_dir directive
@@ -53,13 +10,15 @@ static void checkEmptyString(std::string line)
  */
 t_values	parseUploadDir(std::string line, t_values values)
 {
+	std::string reason = "needs one argument: upload_dir <path>;";
+
 	line = protectedSubstr(line, 10);
 	line = ltrim(line);
-	checkEmptyString(line);
-	checkOneArgumentOnly(line);
-	checkStartingSlash(line);
+	checkEmptyString(line, "upload_dir", reason);
+	checkOneArgumentOnly(line, "upload_dir");
+	checkStartingSlash(line, "upload_dir");
 	line = protectedSubstr(line, 1, line.size() - 1);
-	checkNoEndingSlash(line);
+	checkNoEndingSlash(line, "upload_dir");
 	values.uploadDir = line;
 	return (values);
 }
