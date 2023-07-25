e670d4cc2971345da1a30c3f9faa5a976babb53a
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-07-25 13:54:29 +0200

    remove quotes around text parameter. quotedArgument function also returns false with empty string

--- src/parser/blocks/directives/return.cpp
+++ src/parser/blocks/directives/return.cpp
@@ -4,7 +4,7 @@
 
 static bool quotedArgument(std::string text)
 {
-	if (text.at(0) != '"' || text.size() == 1 || text.back() != '"')
+	if (text.size() < 2 || text.at(0) != '"' || text.back() != '"')
 	{
 		return (false);
 	}
@@ -35,19 +35,27 @@ static void checkEmptyString(std::string line)
  */
 t_values	parseReturn(std::string line, t_values values)
 {
-	std::string code;
+	std::string	code;
+	size_t		firstSpace;
 
 	line = protectedSubstr(line, 6);
 	line = ltrim(line);
 	checkEmptyString(line);
-	code = protectedSubstr(line, 0, firstWhitespace(line));
+	firstSpace = firstWhitespace(line);
+	code = protectedSubstr(line, 0, firstSpace);
 	values.returnCode = parseErrorCode(code, "return");
-	line = protectedSubstr(line, firstWhitespace(line), line.size() - firstWhitespace(line));
+	line = protectedSubstr(line, firstSpace, line.size() - firstSpace);
 	line = ltrim(line);
-	if (line.size() > 0 && firstWhitespace(line) != line.size() && !quotedArgument(line))
+	if (line.size() > 0 && firstWhitespace(line) != line.size() && \
+		!quotedArgument(line))
 	{
-		std::cout << "Error: can't parse return: too many text arguments: [" << line<< "]" << std::endl;
+		std::cout << "Error: can't parse return: too many text arguments: ";
+		std::cout << "[" << line<< "]" << std::endl;
 		exit(EXIT_FAILURE);
+	}
+	if (quotedArgument(line))
+	{
+		line = protectedSubstr(line, 1, line.size() - 2);
 	}
 	values.returnText = line;
 	return (values);
