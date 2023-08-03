8d1851dd9bc37264dfd8cd564f639ae6d1119af9
Jaberkro <jaberkro@student.codam.nl>
2023-08-03 17:07:50 +0200

    cout became cerr

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "webserv.hpp"
+// #include "webserv.hpp"
 #include "parse.hpp"
 #include <iostream>
 #include "Webserver.hpp"
@@ -10,18 +10,18 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		std::cout << "usage: ./webserv [configuration file]" << std::endl;
+		std::cerr << "usage: ./webserv [configuration file]" << std::endl;
 		return (1);
 	}
-	try{
+	// try{
 	parse(servers, argv[1]);
 	// printServers(servers); // turn on to see parsed configuration settings
-	} 
-	catch (const std::exception& e)
-	{
-		std::cerr << "Gaat dit fout?" << '\n';
-		//std::cerr << e.what() << '\n';
-	}
+	// } 
+	// catch (const std::exception& e)
+	// {
+	// 	std::cerr << "Gaat dit fout?" << '\n';
+	// 	//std::cerr << e.what() << '\n';
+	// }
 	std::cout << "Hello World!" << std::endl;
 	//turn the try block of webserv off to test the parsing with make test
 	try {
