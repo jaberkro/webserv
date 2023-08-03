f231588321516ce2fefdf126aa562a01f2016779
Jaberkro <jaberkro@student.codam.nl>
2023-08-03 17:01:27 +0200

    checking if configFile is actually a directory or non-existent

--- src/parser/parse.cpp
+++ src/parser/parse.cpp
@@ -3,15 +3,20 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sys/stat.h>
 
 static bool validExtension(std::string configFile, std::string extension)
 {
+	if (configFile.size() <= extension.size())
+		return (0);
 	for(size_t i = 0; i < extension.size(); i++)
 	{
 		if (configFile.at(configFile.size() - 1 - i) != \
 			extension.at(extension.size() - 1 - i))
 			return (0);
 	}
+	if (configFile.at(configFile.size() - 1 - extension.size()) == '.')
+		return (0);
 	return (1);
 }
 
@@ -23,22 +28,36 @@ static bool validExtension(std::string configFile, std::string extension)
  */
 static std::fstream openFile(char *configFile)
 {
-	std::fstream file;
+	std::fstream	file;
+	struct stat		fileInfo;
 
 	if (!validExtension(configFile, ".conf"))
 	{
-		std::cout << "Error: file extension should be '.conf'" << std::endl;
+		std::cerr << "Error: file extension should be '.conf'" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	if (stat(configFile, &fileInfo) != 0)
+	{
+		std::cerr << "Error: file does not exist" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	if (fileInfo.st_mode & S_IFDIR)
+	{
+		std::cerr << "Error: directory not allowed as input" << std::endl;
 		exit(EXIT_FAILURE);
 	}
 	file.open(configFile, std::fstream::in);
-	if (file.is_open())
-		return (file);
-	std::cout << "Error: not possible to open configuration file" << std::endl;
-	exit(EXIT_FAILURE);
+	if (!file.is_open())
+	{
+		std::cerr << "Error: opening configuration file failed" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	return (file);
 }
 
 /**
- * @brief parse configuration file
+ * @brief 
+ * 
  * 
  * @param servers variable to store the parsed information in
  * @param configFile title of the file to parse
@@ -53,13 +72,18 @@ void parse(std::vector<Server> &servers, char *configFile)
 	{
 		if (line == "")
 			continue ;
-		else if (line == "http {")
+		else if (line == "http {" && servers.size() == 0)
 			parseHTTP(servers, file);
+		else if (line == "http {")
+		{
+			std::cerr << "Error: there can only be one http block" << std::endl;
+			exit(EXIT_FAILURE);
+		}
 		else if (line == "server {" || line.find("location") == 0 || \
 			hasDirective(line) != -1)
 		{
-			std::cout << "Error: [" << line << "]: should be inside ";
-			std::cout << "http block: \nhttp {\n\n}" << std::endl;
+			std::cerr << "Error: [" << line << "]: should be inside ";
+			std::cerr << "http block: \nhttp {\n\n}" << std::endl;
 			exit(EXIT_FAILURE);
 		}
 		else
@@ -67,7 +91,7 @@ void parse(std::vector<Server> &servers, char *configFile)
 	}
 	if (servers.size() == 0)
 	{
-		std::cout << "Error: http block missing: \nhttp {\n\n}" << std::endl; // if you open a directory, you get this error as well. How to catch directories before even opening it?
+		std::cerr << "Error: http block missing: \nhttp {\n\n}" << std::endl; // if you open a directory, you get this error as well. How to catch directories before even opening it?
 		exit(EXIT_FAILURE);
 	}
 }
