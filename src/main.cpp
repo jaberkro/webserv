e2ec0a83031fd8f3ee45ad0d020f111dcea6a6da
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-09-11 18:06:50 +0200

    free(defaultFile); added, comment on catching kqueue() error made

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 void	func_atexit(void)
 {
-	system("leaks webserv");
+	system("leaks -q webserv");
 }
 
 int	main(int argc, char **argv)
@@ -23,14 +23,16 @@ int	main(int argc, char **argv)
 		parse(servers, defaultFile);
 	else
 		parse(servers, argv[1]);
-	// try{
-	printServers(servers); // turn on to see parsed configuration settings
-	// } 
-	// catch (const std::exception& e)
-	// {
-		// std::cerr << "Gaat dit fout?" << '\n';
-		//std::cerr << e.what() << '\n';
-	// }
+	try
+	{
+		printServers(servers); // turn on to see parsed configuration settings
+	} 
+	catch (const std::exception& e)
+	{
+		std::cerr << "printServers does not work correctly" << '\n'; // REMOVE
+		std::cerr << e.what() << '\n';
+	}
+	free (defaultFile);
 	std::cout << "Hello World!" << std::endl;
 	//turn the try block of webserv off to test the parsing with make test
 	try {
@@ -39,7 +41,8 @@ int	main(int argc, char **argv)
 	catch (const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
+		// IF KQUEUE FAILED: ERROR
 	}
-	// atexit(func_atexit);
+	atexit(func_atexit);
 	return (0);
 }
