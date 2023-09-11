43d9debe45dc328e732d5f57fd98e3d939d95b3d
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-09-11 12:07:53 +0200

    questions added about working of root directive that might be depricated

--- src/parser/blocks/directives/root.cpp
+++ src/parser/blocks/directives/root.cpp
@@ -20,16 +20,14 @@ t_values		parseRoot(std::string line, t_values values)
 	checkNotPreviousDirectory(line, "root");
 	if ((line.find("/") != 0 && line.find("\"") != 0) || \
 		(line.find("\"") == 0 && line.size() > 1 && \
-		(line.at(1) != '\"' || line.size() != 2)))
+		(line.at(1) != '\"' || line.size() != 2))) // is this check still needed? root ""; is not allowed right? should be root /;
 	{
 		rootError("path should start with '/'", line);
 	}
-	if (line.find("/") == 0)
+	if (line.find("/") == 0) // is this check still needed? is both root data; and root /data; allowed?
 		line = protectedSubstr(line, 1, line.size() - 1);
 	if (line.size() > 0 && line.find_last_of("/") == line.size() - 1)
-	{
 		rootError("path should not end with '/'", line);
-	}
 	if (line == "")
 		line = ".";
 	values.root = line;
