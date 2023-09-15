ac0dc136d378dd78fcb18cbc94e0f8d5cdd52664
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-09-15 14:53:46 +0200

    removed some tabs

--- include/Response.hpp
+++ include/Response.hpp
@@ -44,24 +44,24 @@ class Response {
 		void	identifyErrorPage(Server const & server);
 		
 		/* getters */
-		size_t											getFileLength(void) const;
-		Request &										getRequest(void);
-		CGI &											getCgi(void);
-		bool											getIsReady(void);
-		int												getStatusCode(void);
-		void											setStatusCode(int code);
-		std::string	&									getFilePath(void);
-		void											setFilePath(std::string path);
-		std::string	&									getPathInfo(void);
-		void											setPathInfo(std::string pathInfo);
-		locIterator	const & 							getLocation(void) const;
-		std::string										getFullResponse(void);
-		std::string										getMessage(void);
-		void											setMessage(std::string);
-		size_t	const & 								getState() const;
-		void											setState(size_t state);
-		void											addToFullResponse(char *response, size_t length);
-		void											addToFullResponse(std::string chunk);
+		size_t				getFileLength(void) const;
+		Request &			getRequest(void);
+		CGI &				getCgi(void);
+		bool				getIsReady(void);
+		int					getStatusCode(void);
+		void				setStatusCode(int code);
+		std::string	&		getFilePath(void);
+		void				setFilePath(std::string path);
+		std::string	&		getPathInfo(void);
+		void				setPathInfo(std::string pathInfo);
+		locIterator	const & getLocation(void) const;
+		std::string			getFullResponse(void);
+		std::string			getMessage(void);
+		void				setMessage(std::string);
+		size_t	const & 	getState() const;
+		void				setState(size_t state);
+		void				addToFullResponse(char *response, size_t length);
+		void				addToFullResponse(std::string chunk);
 
 		/* utils */
 		void	splitUri(std::string const & uri, std::vector<std::string> & chunks);
@@ -69,31 +69,30 @@ class Response {
 
 	private:
 
-		Request									_req;
-		int										_statusCode;
-		size_t									_fileLength;
-		std::string								_filePath;
-		bool									_isReady;
-		locIterator								_location;
-		std::string								_fullResponse;
-		std::string								_message;
-		static std::map<int, std::string>		_responseCodes;
-		size_t									_state;
-		std::string								_pathInfo;
-		
-		CGI										_cgi;
-		size_t									getFileSize(std::string filePath);
-		locIterator								findLocationMatch(std::string target, std::vector<Location> const & locations);
-		locIterator								findExactLocationMatch(std::string target, std::vector<Location> const & locations);
-		locIterator								findWildcardLocationMatch(std::string target, std::vector<Location> const & locations);
-		locIterator								findClosestLocationMatch(std::string target, std::vector<Location> const & locations);
-		std::string								findIndexPage(locIterator itLoc);
-		void									extractPathInfo(std::string & targetUri);
-		void									prepareFirstLine(void);
-		void									prepareHeaders(std::string const & root);
-		void									prepareContent(std::ifstream &file);
-		void									executeCgiScript(void);
-
+		Request								_req;
+		int									_statusCode;
+		size_t								_fileLength;
+		std::string							_filePath;
+		bool								_isReady;
+		locIterator							_location;
+		std::string							_fullResponse;
+		std::string							_message;
+		static std::map<int, std::string>	_responseCodes;
+		size_t								_state;
+		std::string							_pathInfo;
+		CGI									_cgi;
+	
+		size_t		getFileSize(std::string filePath);
+		locIterator	findLocationMatch(std::string target, std::vector<Location> const & locations);
+		locIterator	findExactLocationMatch(std::string target, std::vector<Location> const & locations);
+		locIterator	findWildcardLocationMatch(std::string target, std::vector<Location> const & locations);
+		locIterator	findClosestLocationMatch(std::string target, std::vector<Location> const & locations);
+		std::string	findIndexPage(locIterator itLoc);
+		void		extractPathInfo(std::string & targetUri);
+		void		prepareFirstLine(void);
+		void		prepareHeaders(std::string const & root);
+		void		prepareContent(std::ifstream &file);
+		void		executeCgiScript(void);
 };
 
 // std::string	deleteFile(Request request, locIterator const & location);
