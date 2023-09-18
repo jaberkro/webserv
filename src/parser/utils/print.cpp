7726b0e317fe569f68c1c004ff15b28f97ec1638
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-09-18 17:16:47 +0200

    5 functions of max 80 chars per line and 25 lines now

--- src/parser/utils/print.cpp
+++ src/parser/utils/print.cpp
@@ -4,6 +4,49 @@
 #include <map>
 #include <iostream>
 
+static void printReturn(Location location)
+{
+	if (location.getReturnCode() > 0)
+	{
+		std::cout << "\n\t\treturn: [" << location.getReturnCode() << "]";
+		if (location.getReturnLink() != "")
+			std::cout << " [" << location.getReturnLink() << "]";
+		else if (location.getReturnLink() != "")
+			std::cout << " [" << location.getReturnLink() << "]";
+	}
+}
+
+static void printAllowedDenied(Location location)
+{
+	if (location.getAllowed().size() != 0)
+	{
+		std::cout << "\n\t\tallowed:";
+		for (size_t k = 0; k < location.getAllowed().size(); k++)
+			std::cout << " [" << location.getAllow(k) << "]";
+	}
+	if (location.getDenied().size() != 0)
+	{
+		std::cout << "\n\t\tdenied:";
+		for (size_t k = 0; k < location.getDenied().size(); k++)
+			std::cout << " [" << location.getDeny(k) << "]";
+	}
+}
+
+static void	printErrorPages(std::map<int, std::string> errorPages)
+{
+	if (!errorPages.empty())
+	{
+		std::cout << "\n\terror_page: ";
+		std::cout << "size: " << errorPages.size();
+		for (std::map<int, std::string>::iterator sit= errorPages.begin(); \
+		sit!=errorPages.end(); ++sit)
+		{
+			std::cout << "\n\t\t[" << sit->first << "] => [";
+			std::cout << sit->second << "]";
+		}
+	}
+}
+
 void	printLocation(Location location)
 {
 	std::cout << "\n\tlocation ";
@@ -20,34 +63,13 @@ void	printLocation(Location location)
 	if (location.getAutoindex() == true)
 		std::cout << "\n\t\tautoindex: on";
 	if (location.getMaxBodySize() != 1000000)
-		std::cout << "\n\t\tclient_max_body_size: [" << location.getMaxBodySize() << "]";
-	std::map<int, std::string> errorPages = location.getErrorPages();
-	if (!errorPages.empty())
 	{
-		std::cout << "\n\t\terror_page: ";
-		for (std::map<int, std::string>::iterator it= errorPages.begin(); it!=errorPages.end(); ++it)
-			std::cout << "\n\t\t\t[" << it->first << "] => [" << it->second << "]";
+		std::cout << "\n\t\tclient_max_body_size: [";
+		std::cout << location.getMaxBodySize() << "]";
 	}
-	if (location.getAllowed().size() != 0)
-	{
-		std::cout << "\n\t\tallowed:";
-		for (size_t k = 0; k < location.getAllowed().size(); k++)
-			std::cout << " [" << location.getAllow(k) << "]";
-	}
-	if (location.getDenied().size() != 0)
-	{
-		std::cout << "\n\t\tdenied:";
-		for (size_t k = 0; k < location.getDenied().size(); k++)
-			std::cout << " [" << location.getDeny(k) << "]";
-	}
-	if (location.getReturnCode() > 0)
-	{
-		std::cout << "\n\t\treturn: [" << location.getReturnCode() << "]";
-		if (location.getReturnLink() != "")
-			std::cout << " [" << location.getReturnLink() << "]";
-		else if (location.getReturnLink() != "")
-			std::cout << " [" << location.getReturnLink() << "]";
-	}
+	printErrorPages(location.getErrorPages());
+	printAllowedDenied(location);
+	printReturn(location);
 	if (location.getUploadDir().size() != 0)
 		std::cout << "\n\t\tupload_dir: [" << location.getUploadDir() << "]";
 }
@@ -67,14 +89,7 @@ void	printServers(std::vector<Server> servers)
 			std::cout << "\n\tserver_name:";
 		for (size_t j = 0; j < servers.at(i).getServerNames().size(); j++)
 			std::cout << " [" << servers.at(i).getServerName(j) << "]";
-		std::map<int, std::string> errorPages = servers.at(i).getErrorPages();
-		if (!errorPages.empty())
-		{
-			std::cout << "\n\terror_page: ";
-			std::cout << "size: " << errorPages.size();
-			for (std::map<int, std::string>::iterator sit= errorPages.begin(); sit!=errorPages.end(); ++sit)
-				std::cout << "\n\t\t[" << sit->first << "] => [" << sit->second << "]";
-		}
+		printErrorPages(servers.at(i).getErrorPages());
 		for (size_t j = 0; j < servers.at(i).getLocations().size(); j++)
 		{
 			printLocation(servers.at(i).getLocation(j));
