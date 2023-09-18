129d2e460817a543bdf791b00ff2f4ff9d5ac64f
Jorien Aberkrom <jaberkro@student.codam.nl>
2023-09-18 15:31:26 +0200

    comment added

--- src/response/CGI.cpp
+++ src/response/CGI.cpp
@@ -109,17 +109,17 @@ void	CGI::cgiWrite(Response & response)
 	ssize_t bytesSent;
 	ssize_t chunkSize = std::min(this->_req.getBody().length(), static_cast<size_t>(MAXLINE));
 	bytesSent = write(_webservToScript[W], this->_req.getBody().c_str(), chunkSize);
-	std::cerr << "[writing to cgi] chunk size is " << chunkSize << ", BytesSent is " << bytesSent << std::endl;
-	if (bytesSent > 0) //JMA: this if statement is important!
+	// std::cerr << "[writing to cgi] chunk size is " << chunkSize << ", BytesSent is " << bytesSent << std::endl;
+	if (bytesSent > 0)
 		this->_req.setBody(this->_req.getBody().erase(0, bytesSent));
 	if (this->_req.getBody().size() == 0 || bytesSent == 0)
 	{
-		std::cerr << "[writing to cgi] body size is " << this->_req.getBody().size() << ", BytesSent is " << bytesSent << std::endl;
+		// std::cerr << "[writing to cgi] body size is " << this->_req.getBody().size() << ", BytesSent is " << bytesSent << std::endl;
 		response.setState(READ_CGI);
 		close(this->_scriptToWebserv[W]);
 		close(this->_webservToScript[R]);
 		close(this->_webservToScript[W]);
-		std::cout << "Closing webservToScript[W]" << std::endl;
+		// std::cout << "Closing webservToScript[W]" << std::endl;
 	}
 }
 
@@ -150,7 +150,7 @@ void	CGI::cgiRead(Response & response, std::string & fullResponse)
 	// {
 		if ((bytesRead = read(this->_scriptToWebserv[R], &buf, RESPONSELINE)) > 0)
 		{
-			std::cout << "Read call for cgi, bytesRead = " << bytesRead << std::endl;
+			// std::cout << "Read call for cgi, bytesRead = " << bytesRead << std::endl;
 			std::string	chunk(buf, bytesRead);
 			response.addToFullResponse(chunk);
 		}
@@ -159,7 +159,7 @@ void	CGI::cgiRead(Response & response, std::string & fullResponse)
 			close(this->_scriptToWebserv[R]);
 			if (WIFEXITED(this->_childProcessExitStatus)) {
 				exitCode = WEXITSTATUS(this->_childProcessExitStatus);
-				std::cout << "Script exited with exit code " << exitCode << " (so " << exitCode + 256 << ")" << std::endl;
+				// std::cout << "Script exited with exit code " << exitCode << " (so " << exitCode + 256 << ")" << std::endl;
 				if (exitCode > 0)
 				{
 					response.setStatusCode(exitCode == 1 ? INTERNAL_SERVER_ERROR : exitCode + 256);
